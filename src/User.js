d8b5b42338b72199a87233f09f0f214e7fb9fafa
singh3069 <73482745+singh3069@users.noreply.github.com>
2021-10-03 19:03:45 +0530

    user data is displayed  user can nevigate btw pages through buttons

---
+++ src/User.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
+
+function User() {
+  const [user, setUser] = useState([]);
+  const [dataPage, setDataPage] = useState("0");
+  const pageCounts = 3;
+
+  useEffect(() => {
+    const url = `https:reqres.in/api/users?page=${dataPage}`;
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setUser(json.data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, [dataPage]);
+
+  const changePage = ({ selected }) => {
+    setDataPage(selected);
+  };
+  return (
+    <div className="allUserDiv">
+      {/* <h1>User Info</h1> */}
+      {user.map((userinfo) => {
+        return (
+          <div key={userinfo.id}>
+            <img src={userinfo.avatar} alt="userImage" />
+            <h3>{userinfo.first_name}</h3>
+            <p>{userinfo.email}</p>
+          </div>
+        );
+      })}
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCounts}
+        onPageChange={changePage}
+        containerClassName={"paginationContainer"}
+        previousLinkClassName={"previousBttn"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"disabledBttn"}
+        activeClassName={"activeBttn"}
+      />
+    </div>
+  );
+}
+
+export default User;
